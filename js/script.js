dffd114d1622607d5c87caf51f55ca8f53045623
MelissaP2312 <al062710@uacam.mx>
2024-10-18 21:15:47 -0600

    Login 3/4

--- js/script.js
+++ js/script.js
@@ -1,37 +1,50 @@
 $(document).ready(function() {
-    $('#password').on('keyup', function() {
-        var password = $(this).val();
-        var passwordHelp = $('#passwordHelp');
+    $('.btn.btn-danger.btn-md:contains("Registrar")').on('click', function(e) {
+        $('#login-container').hide();
+        $('#register-container').show();
+    });
+    
+    const passwordField = document.getElementById("userPassword");
+    const togglePassword = document.getElementById("togglePassword");
+    const icon = togglePassword.querySelector("i");
+
+    togglePassword.addEventListener("click", function() {
+        const type = passwordField.type === "password" ? "text" : "password";
+        passwordField.type = type;
+        icon.classList.toggle("fa-eye");
+        icon.classList.toggle("fa-eye-slash");
+    });
+    
+    $('#password, #password-confirm').on('keyup', function() {
+        validatePasswordStrength();
+        validatePasswordsMatch();
+    });
+
+    function validatePasswordStrength() {
+        var password = $('#password').val();
+        var passwordInput = document.getElementById('password');
 
         // Expresión regular para verificar la seguridad de la contraseña
         var regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,12}$/;
 
+        //Valida si se cumple el nivel de la contraseña
         if (!regex.test(password)) {
-            passwordHelp.text('La contraseña debe tener entre 8 y 12 caracteres, con al menos una letra mayúscula, una letra minúscula, un número y un carácter especial.');
-            $('#registerBtn').attr('disabled', true);
+            passwordInput.setCustomValidity('La contraseña debe tener entre 8 y 12 caracteres, con al menos una letra mayúscula, una letra minúscula, un número y un carácter especial.');
         } else {
-            passwordHelp.text('');
-            // Verificar si las contraseñas coinciden
-            validatePasswordsMatch();
+            passwordInput.setCustomValidity('');
         }
-    });
-
-    $('#password-confirm').on('keyup', function() {
-        // Verificar si las contraseñas coinciden
-        validatePasswordsMatch();
-    });
+    }
 
     function validatePasswordsMatch() {
         var password = $('#password').val();
         var confirmPassword = $('#password-confirm').val();
-        var passwordMatchHelp = $('#passwordMatchHelp');
+        var confirmPasswordInput = document.getElementById('password-confirm');
 
+        //Valida que las contraseñas coincidan
         if (password !== confirmPassword) {
-            passwordMatchHelp.text('Las contraseñas no coinciden.');
-            $('#registerBtn').attr('disabled', true);
+            confirmPasswordInput.setCustomValidity('Las contraseñas no coinciden.');
         } else {
-            passwordMatchHelp.text('');
-            $('#registerBtn').attr('disabled', false); // Habilitar el botón si todo está bien
+            confirmPasswordInput.setCustomValidity('');
         }
     }
 });
