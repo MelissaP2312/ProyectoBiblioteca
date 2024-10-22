b4c3a6d29030620cc3416343fd7138f7953e6f66
MelissaP2312 <al062710@uacam.mx>
2024-10-22 10:18:50 -0600

    Login listo

--- php/login.php
+++ php/login.php
@@ -11,8 +11,8 @@ try {
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        $correo = $_POST['email'] ?? null;
-        $contraseña = $_POST['password'] ?? null;
+        $correo = $_POST['correo'] ?? null;
+        $contraseña = $_POST['contraseña'] ?? null;
 
         // Búsqueda en la base de datos
         try {
@@ -22,11 +22,19 @@ try {
 
             if ($resultado && password_verify($contraseña, $resultado['contraseña'])) {
                 // La contraseña ingresada coincide con el hash almacenado
-                echo "Usuario loggeado.";
+                $response['status'] = 'success';
+                $response['message'] = 'Usuario loggeado.';
+                // Redirigir a otra vista HTML (por ejemplo, "home.html")
+                header('Location: ../html/main.html');
+                exit(); // Asegúrate de hacer exit después de header
             } else {
                 // La contraseña o el correo no son correctos
                 echo "Correo o contraseña incorrectos.";
             }
+            if ($resultado === false) {
+                echo "No se encontró un usuario con ese correo.";
+            }
+            
         } catch (PDOException $e) {
             echo "Error en la consulta SQL: " . $stmt->queryString . "<br>";
             echo "Error: " . $e->getMessage();
